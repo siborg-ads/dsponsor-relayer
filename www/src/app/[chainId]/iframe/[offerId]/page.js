fe501b5fe56776f0b63ed5e6b275b7a71c61b47f
Alex Werner <obusco@gmail.com>
2024-04-30 19:45:42 +0200

    feat: default link when unknown

--- www/src/app/[chainId]/iframe/[offerId]/page.js
+++ www/src/app/[chainId]/iframe/[offerId]/page.js
@@ -72,7 +72,9 @@ const AdPlaceholder = () => {
     return (
         <div
             className="aspect-square w-full border border-blue-500 overflow-hidden flex justify-center items-center bg-[#00143e] text-black rounded hover:bg-[#353f75] hover:border-[#9abffb]">
-            <img src="/available.webp" alt="Ad image" className="object-contain w-full h-full"/>
+            <a href="#" target="_blank">
+                 <img src="/available.webp" alt="Ad image" className="object-contain w-full h-full"/>
+            </a>
         </div>
     );
 };
