7f1b00fc6a0989ba6c9e009c3d1914749512f52a
Alex Werner <obusco@gmail.com>
2024-05-07 01:27:01 +0200

    fix: specify format type

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -26,7 +26,7 @@ const nextConfig = {
                 source: "/:chainId/ad/:offerId/:tokenId/image",
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: 'Content-Type', value: 'image' },
+                    { key: 'Content-Type', value: 'image/png' },
                     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
