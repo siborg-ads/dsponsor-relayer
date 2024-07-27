52f2e6cb7dd901d8a8783e7bcf878f6fc6553afb
Anthony <10244943+antho31@users.noreply.github.com>
2024-07-27 19:55:54 +0200

    feat: add working ofac & native thresold shield3 detection

--- src/app/api/[chainId]/prices/route.js
+++ src/app/api/[chainId]/prices/route.js
@@ -9,12 +9,13 @@ export async function GET(request, context) {
   const token = searchParams.get("token");
   const amount = searchParams.get("amount");
   const slippage = searchParams.get("slippage");
+  const recipient = searchParams.get("recipient");
 
   const tokenOutAddr = token ? token : config?.[chainId]?.smartContracts?.USDC?.address;
   const amountOut = amount ? amount : "1000000";
   const slippagePerCent = slippage && Number(slippage) > 0.01 ? Number(slippage) : 0.3;
 
-  const result = await getEthQuote(chainId, tokenOutAddr, amountOut, slippagePerCent);
+  const result = await getEthQuote(chainId, tokenOutAddr, amountOut, slippagePerCent, recipient);
 
   if (!result) {
     return new Response("Invalid info provided", {
--- src/queries/uniswap/quote.js
+++ src/queries/uniswap/quote.js
@@ -1,9 +1,15 @@
 import config from "@/config";
 import { priceFormattedForAllValuesObject } from "@/utils/string";
-import { Transaction, ethers, getAddress } from "ethers";
+import { Transaction, ethers, getAddress, isAddress } from "ethers";
 import Quoter from "@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json";
 
-export const getEthQuote = async (chainId, tokenOutAddr, amountOut, slippagePerCent = 0.3) => {
+export const getEthQuote = async (
+  chainId,
+  tokenOutAddr,
+  amountOut,
+  slippagePerCent = 0.3,
+  recipient
+) => {
   const uniswapV3QuoterAddr = config?.[chainId]?.smartContracts?.UNISWAP_QUOTER?.address;
   const WNATIVE_ADDR = config?.[chainId]?.smartContracts?.WNATIVE?.address;
   const USDC_ADDR = config?.[chainId]?.smartContracts?.USDC?.address;
@@ -30,9 +36,7 @@ export const getEthQuote = async (chainId, tokenOutAddr, amountOut, slippagePerC
     if (supportedCurrencies.includes(tokenOutAddr)) {
       const provider = new ethers.JsonRpcProvider(rpcURL);
       const signer = ethers.Wallet.createRandom().connect(provider);
-      const from = await signer.getAddress();
-      // "0x6aabdd49a7f97f5242fd0fd6938987e039827666" // forta block
-      // "0x098b716b8aaf21512996dc57eb0615e2383e2f96" // ofac blocked
+
       const quoterContract = new ethers.Contract(uniswapV3QuoterAddr, Quoter.abi, signer);
 
       const ethQuotePayload = {
@@ -42,26 +46,6 @@ export const getEthQuote = async (chainId, tokenOutAddr, amountOut, slippagePerC
         amount: amountOut,
         sqrtPriceLimitX96: 0
       };
-
-      // Check if the transaction will be secure
-      // we activated Uniswap trade slippage thresold, Forta Scam & OFAC policies
-      try {
-        const tx = await quoterContract.quoteExactOutputSingle.populateTransaction(
-          ethQuotePayload,
-          {
-            chainId,
-            value: 0
-          }
-        );
-
-        const serializedTx = Transaction.from(tx).unsignedSerialized;
-        const params = [serializedTx, from];
-
-        const rpcResponse = await provider.send("eth_simulateTransaction", params);
-        shield3Decisions = rpcResponse?.transaction?.decisionReasons;
-      } catch {
-        //
-      }
 
       [amountInEth] =
         tokenOutAddr == WNATIVE_ADDR
@@ -83,6 +67,81 @@ export const getEthQuote = async (chainId, tokenOutAddr, amountOut, slippagePerC
         tokenOutAddr == WNATIVE_ADDR
           ? amountInEth
           : (amountInEth * BigInt(slippage.toString())) / BigInt(slippageMul.toString());
+
+      try {
+        // Check if the transaction will be secure
+        // - we activated Uniswap trade slippage & amounts thresolds + Forta Scam & OFAC policies
+        // - it only works with Native thresold policy (and not ERC20 value & uniswap slippage)
+        //
+        // TEST values:
+        // recipient = "0x6aabdd49a7f97f5242fd0fd6938987e039827666" // forta block
+        // recipient = "0x098b716b8aaf21512996dc57eb0615e2383e2f96"; // ofac blocked
+
+        const swapRouterABI = [
+          {
+            inputs: [
+              {
+                components: [
+                  { internalType: "address", name: "tokenIn", type: "address" },
+                  { internalType: "address", name: "tokenOut", type: "address" },
+                  { internalType: "uint24", name: "fee", type: "uint24" },
+                  { internalType: "address", name: "recipient", type: "address" },
+                  { internalType: "uint256", name: "amountOut", type: "uint256" },
+                  { internalType: "uint256", name: "amountInMaximum", type: "uint256" },
+                  { internalType: "uint160", name: "sqrtPriceLimitX96", type: "uint160" }
+                ],
+                internalType: "struct IV3SwapRouter.ExactOutputSingleParams",
+                name: "params",
+                type: "tuple"
+              }
+            ],
+            name: "exactOutputSingle",
+            outputs: [{ internalType: "uint256", name: "amountIn", type: "uint256" }],
+            stateMutability: "payable",
+            type: "function"
+          }
+        ];
+        const swapRouterAddr = config?.[chainId]?.smartContracts?.UNISWAP_SWAP_ROUTER?.address;
+        const shield3RpcUrl = config?.[chainId]?.shield3RpcURL;
+        const shield3Provider = new ethers.JsonRpcProvider(shield3RpcUrl);
+        const shield3Signer = ethers.Wallet.createRandom().connect(shield3Provider);
+
+        const swapRouterContract = new ethers.Contract(
+          swapRouterAddr,
+          swapRouterABI,
+          shield3Signer
+        );
+
+        recipient = isAddress(recipient) ? recipient : await shield3Signer.getAddress();
+
+        const tx = await swapRouterContract.exactOutputSingle.populateTransaction(
+          {
+            tokenIn: WNATIVE_ADDR,
+
+            // trick: if tokenOut is WETH, there is a wrap instead of swap,
+            // we use USDC to have low probability to have slippage issue
+            tokenOut: tokenOutAddr == WNATIVE_ADDR ? tokenOutAddr : USDC_ADDR,
+
+            fee: 3000,
+            recipient,
+            amountOut: amountUSDC,
+            amountInMaximum: amountInEthWithSlippage,
+            sqrtPriceLimitX96: 0
+          },
+          {
+            chainId,
+            value: amountInEthWithSlippage
+          }
+        );
+
+        const serializedTx = Transaction.from(tx).unsignedSerialized;
+        const params = [serializedTx, recipient];
+
+        const rpcResponse = await shield3Provider.send("eth_simulateTransaction", params);
+        shield3Decisions = rpcResponse?.transaction?.decisionReasons;
+      } catch (e) {
+        //  console.error("Shield3 error", e);
+      }
     }
   } catch (e) {
     console.error("Quote error", chainId, tokenOutAddr, amountOut, slippagePerCent);
