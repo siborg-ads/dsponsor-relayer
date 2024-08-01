125b6dc23a82433426f8d588264a5a23a49543ad
Anthony <10244943+antho31@users.noreply.github.com>
2024-08-01 19:27:43 +0200

    fix: redirect for img integration

--- src/app/[chainId]/integrations/[offerId]/[tokenId]/image/route.js
+++ src/app/[chainId]/integrations/[offerId]/[tokenId]/image/route.js
@@ -1,3 +1,4 @@
+/*
 import { NextResponse } from "next/server";
 import { getAdDataForToken } from "@/queries/ads";
 import { isValidUrl } from "@/utils";
@@ -27,6 +28,50 @@ export async function GET(request, context) {
       const headers = new Headers();
       headers.set("Content-Type", "image/*");
       return new NextResponse(blob, { status: 200, statusText: "OK", headers });
+    } else {
+      return new Response("Invalid image URL", {
+        status: 400
+      });
+    }
+  } catch (e) {
+    console.error("Error fetching image", imgUrl);
+    return new Response("Error fetching image", {
+      status: 500
+    });
+  }
+}
+*/
+
+import { NextResponse } from "next/server";
+import { getRandomAdData } from "@/queries/ads";
+import { isValidUrl } from "@/utils";
+
+export async function GET(request, context) {
+  const { offerId, chainId } = context.params;
+  const requestUrl = new URL(`${request.url}`);
+  const searchParams = requestUrl.searchParams;
+  const tokenIds = searchParams.get("tokenIds");
+  let ratio = searchParams.get("ratio");
+  ratio = ratio?.length && /^\d+:\d+$/.test(ratio) ? ratio : "1:1";
+
+  let imgUrl;
+
+  const { randomAd, _adParameterIds } =
+    (await getRandomAdData({
+      chainId,
+      adOfferId: offerId,
+      tokenIds: tokenIds?.split(","),
+      adParameterIds: [`imageURL-${ratio}`]
+    })) || {};
+
+  if (randomAd) {
+    const [imageKey] = _adParameterIds;
+    imgUrl = randomAd[imageKey].data;
+  }
+
+  try {
+    if (isValidUrl(imgUrl)) {
+      return NextResponse.redirect(imgUrl, 307);
     } else {
       return new Response("Invalid image URL", {
         status: 400
